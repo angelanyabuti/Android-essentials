ef21e22de2e94e66c9f82e412a56623be79e5f4a
Angela <nyabutiangella@gmail.com>
2025-01-08 10:51:17 +0300

    Used when statement for multiple branches.

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -1,12 +1,16 @@
 fun main() {
-    val trafficLightColor = "Black"
-    if ( trafficLightColor == "Red") {
-        println("Stop")
-    }else if ( trafficLightColor == "Amber") {
-        println("Get Ready")
-    }else if (trafficLightColor == "Green"){
-        println("Go")
-    }else{
-        println("Invalid Color")
+    val trafficLightColor = "Red"
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Amber" -> println("Get Ready")
+        "Green" -> println("Go")
+        else -> println("Invalid color")
+    }
+    val x:Any = 20 //indicates x can be of any value other than int
+    when (x){
+        2,3,5,7 -> println("x is a prime number between 1 and 10")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number")
+        is Int -> println("x is an integer number but not between 1 and 10")
+        else -> println("x isn't an integer number.")
     }
 }

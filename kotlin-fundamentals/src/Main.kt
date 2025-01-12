5ca27e84acbb04061245dfcceb693567a04fffdf
Angela <nyabutiangella@gmail.com>
2025-01-12 13:35:03 +0300

    Using the not-null assertion operator (!!.).

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    var favouriteColor: String ? = "Maroon"
-    println(favouriteColor?.length)  //accessing properties of nullable variables
+    var favouriteColor: String ? = "maroon"
+    println(favouriteColor!!.length)  //accessing properties of nullable variables
 }

8b69a9c105001f97e02a85cf04aa05d22d2e428d
Angela <nyabutiangella@gmail.com>
2025-01-15 10:44:28 +0300

    Defining class methods.

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -1,6 +1,13 @@
 class SmartDevice {
-
+    fun turnOn() {
+        println("Smart device has been turned on")
+    }
+    fun turnOff() {
+        println("Smart device has been turned off")
+    }
 }
 fun main() {
     val smartTvDevice = SmartDevice()
+    smartTvDevice.turnOn() //calling the turn on method
+    smartTvDevice.turnOff() //calling the turn off method
 }

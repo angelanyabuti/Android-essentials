067150d091f6d31e26b12789abd824a68b304b01
Angela <nyabutiangella@gmail.com>
2025-01-15 10:15:35 +0300

    Defining a class.

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -1,7 +1,6 @@
+class SmartDevice {
+
+}
 fun main() {
-    var favouriteColor: String? = "maroon"
 
-    val length = favouriteColor?.length ?: 0
-
-    println("The length of your favourite color's name is $length")
 }

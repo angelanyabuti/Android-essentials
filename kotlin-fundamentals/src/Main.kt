b8514a523d78e3d1bb7a1f26eaa3f1ca6d7b88a8
Angela <nyabutiangella@gmail.com>
2025-01-21 09:43:44 +0300

    Relationships between classes.

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -1,4 +1,4 @@
-class SmartDevice(val name: String, val category: String) {//primary constructor for the class smartDevice
+open class SmartDevice(val name: String, val category: String) {//primary constructor for the class smartDevice
     var deviceStatus = "online"
     //secondary constructor initialization                          //initialize primary constructor
     constructor(name: String, category: String, statusCode: Int) : this(name, category) {
@@ -9,9 +9,9 @@ class SmartDevice(val name: String, val category: String) {//primary constructor
         }
 
     }
-    var speakerVolume: Int
+    /*var speakerVolume: Int
         get() = 2
-        set(value) = TODO()
+        set(value) = TODO()*/
 
     fun turnOn() {
         println("Smart device has been turned on")
@@ -20,6 +20,90 @@ class SmartDevice(val name: String, val category: String) {//primary constructor
         println("Smart device has been turned off")
     }
 }
+//smarttv subclass
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+        SmartDevice(name = deviceName, category = deviceCategory){
+            var speakerVolume = 2
+                set(value) {
+                    if (value in 0..100)
+                        field = value
+                }
+            var channelNumber = 1
+                set(value) {
+                    if (value in 0..200)
+                        field = value
+                }
+            fun increaseSpeakerVolume() {
+                speakerVolume++
+                println("Speaker volume increased to $speakerVolume")
+            }
+            fun nextChannel() {
+                channelNumber++
+                println("Speaker volume increased to $speakerVolume")
+            }
+        }
+
+//smartlight subclass
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+      SmartDevice(name = deviceName, category = deviceCategory) {
+          var brightnessLevel = 0
+              set(value)  {
+                  if (value in 0..100) {
+                      field = value
+                  }
+              }
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness level has been increased to $brightnessLevel")
+    }
+      }
+
+//Has - A relationship
+//The SmartHome class HAS-A smart TV device
+class SmartHome(
+    val smartTvDevice: SmartTvDevice,
+    val SmartLightDevice: SmartLightDevice
+){
+    //calls the turnOn() method on the smartTvDevice
+    fun turnOnTv() {
+        smartTvDevice.turnOn()
+    }
+    //calls the turnOff() method on the smartTvDevice
+    fun turnOffTv() {
+        smartTvDevice.turnOff()
+    }
+    //calls the increaseSpeakerVolume() method on the smartTvDevice
+    fun increaseTvVolume() {
+        smartTvDevice.increaseSpeakerVolume()
+    }
+    //calls the nextChannel() method on the smartTvDevice
+    fun changeTvChannelToNext() {
+        smartTvDevice.nextChannel()
+    }
+    //calls the turnOn() method on the smartLightDevice object
+    fun turnOnLight() {
+        SmartLightDevice.turnOn()
+    }
+    //calls the turnoff method on the SmartLightDevice object
+    fun  turnOffLight() {
+        SmartLightDevice.turnOff()
+    }
+    //calls the increaseBrightness() method
+    fun increaseLightBrightness() {
+        SmartLightDevice.increaseBrightness()
+    }
+    //calls the turnOffTv() and turnOffLight() methods
+    fun turnOffAllDevices() {
+        turnOffTv()
+        turnOffLight()
+    }
+}
+
+
+
+
+
+
 fun main() {
     val smartTvDevice = SmartDevice(name = "Android TV", category = "Entertainment")
     println("Device name is: ${smartTvDevice.name}")

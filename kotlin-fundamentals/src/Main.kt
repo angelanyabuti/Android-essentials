fb05696b4764be6c71f67b4d12230bfbd43bc6d3
Angela <nyabutiangella@gmail.com>
2025-01-30 13:43:31 +0300

    Define property delegates.

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -1,5 +1,7 @@
-//visibility modifiers for constructors
-open class SmartDevice protected constructor(val name: String, val category: String) {//primary constructor for the class smartDevice
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty //this is an interface that represents a declared property and lets you access the metadata on a delegated property
+
+open class SmartDevice(val name: String, val category: String) {//primary constructor for the class smartDevice
     var deviceStatus = "online"
     protected set
 
@@ -29,16 +31,9 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
         SmartDevice(name = deviceName, category = deviceCategory){
 
         override val deviceType = "Smart TV"
-            var speakerVolume = 2
-                set(value) {
-                    if (value in 0..100)
-                        field = value
-                }
-            var channelNumber = 1
-                set(value) {
-                    if (value in 0..200)
-                        field = value
-                }
+            private var speakerVolume by RangeRegulator(initialValue = 2, minValue = 0, maxValue = 100)
+
+            private var channelNumber by RangeRegulator(initialValue = 1, minValue = 0, maxValue = 200)
             fun increaseSpeakerVolume() {
                 speakerVolume++
                 println("Speaker volume increased to $speakerVolume")
@@ -61,12 +56,7 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
 class SmartLightDevice(deviceName: String, deviceCategory: String) :
       SmartDevice(name = deviceName, category = deviceCategory) {
     override val deviceType = "Smart Light"
-          var brightnessLevel = 0
-              set(value)  {
-                  if (value in 0..100) {
-                      field = value
-                  }
-              }
+         private var brightnessLevel by RangeRegulator(initialValue = 0, minValue = 0, maxValue = 100 )
         fun increaseBrightness() {
             brightnessLevel++
             println("Brightness level has been increased to $brightnessLevel")
@@ -131,17 +121,31 @@ class SmartHome(
     }
 }
 
+class RangeRegulator(initialValue: Int, private val minValue: Int, private val maxValue: Int) : ReadWriteProperty<Any?, Int> {
+    var fieldData = initialValue //acts as the backing field for the variable
+    //these methods act as the properties' getter and setter functions
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return fieldData
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        if (value in minValue..maxValue) {
+            fieldData = value
+        }
+    }
+
+}
 
 
 
 
 
 fun main() {
-    //val smartTvDevice = SmartDevice(name = "Android TV", category = "Entertainment")
+    val smartTvDevice = SmartDevice(name = "Android TV", category = "Entertainment")
     var smartDevice: SmartDevice = SmartTvDevice("Android TV","Entertainment")
     //println("Device name is: ${smartTvDevice.name}")
     smartDevice.turnOn() //calling the turn on method
-   // smartTvDevice.turnOff() //calling the turn off method
+    smartTvDevice.turnOff() //calling the turn off method
     smartDevice = SmartLightDevice("Google Light", "Utility")
     smartDevice.turnOn()
 }

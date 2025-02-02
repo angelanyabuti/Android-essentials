3b96aa220c12fcaa46b8f8088cdaecb9f1d421df
Angela <nyabutiangella@gmail.com>
2025-02-02 13:26:47 +0300

    Using Functions as a datatype.

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -2,7 +2,12 @@ fun main() {
     val trickFunction =  trick //you do not add brackets because we are not calling the function
     trick()
     trickFunction()
+    treat()
 }
 val trick = {
     println("No treats")
+}
+//function with lambda expression
+val treat: () -> Unit = { //specifying treat variable's datatype as () -> Unit
+    println("Have a treat")
 }

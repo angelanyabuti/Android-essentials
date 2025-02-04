b041470180777cb3353f02204ffd3143fa7591e0
Angela <nyabutiangella@gmail.com>
2025-02-04 21:23:52 +0300

    Passing a function to another function as an argument.

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -1,14 +1,21 @@
 fun main() {
-    val treatFunction = trickOrTreat(false)
-    val trickFunction = trickOrTreat(true)
+    val coins: (Int) -> String = { quantity ->
+        "$quantity quarters"
+    }
+    val cupcake: (Int) -> String = {
+        "Have a cupcake!"
+    }
+    val treatFunction = trickOrTreat(false, coins)
+    val trickFunction = trickOrTreat(true, cupcake)
     treatFunction()
     trickFunction()
 }
 //Using a function as a return type
-fun trickOrTreat(isTrick: Boolean): () -> Unit {
+fun trickOrTreat(isTrick: Boolean, extraTreat: (Int) -> String): () -> Unit {
     if (isTrick){
         return trick
     }else {
+        println(extraTreat(5))
         return treat
     }
 }

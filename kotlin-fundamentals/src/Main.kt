de16b0ef1f26879346cd4d165ed2106f05397ceb
Angela Nyabuti <nyabutiangella@gmail.com>
2025-02-10 10:56:41 +0300

    Revert "Mobile notifications."

--- kotlin-fundamentals/src/Main.kt
+++ kotlin-fundamentals/src/Main.kt
@@ -1,17 +1,29 @@
 fun main() {
-    val morningNotification = 51
-    val eveningNotification = 135
-
-    printNotificationSummary(morningNotification)
-    printNotificationSummary(eveningNotification)
-}
-
-//print notifications function
-fun printNotificationSummary(numberOfMessages: Int) {
-    if (numberOfMessages > 99) {
-        println("Your phone is blowing up. You have 99+ notifications")
-    } else {
-        println("You have $numberOfMessages notifications")
+    /*val coins: (Int) -> String = { //omitting the parameter name and using it
+        "$it quarters"
+    }*/
+    val treatFunction = trickOrTreat(false) {"$it quarters"} //using trailing lambda syntax
+    val trickFunction = trickOrTreat(true, null)
+    trickFunction()
+    repeat(4){//repeat function
+        treatFunction()
     }
-
+}
+//Using a function as a return type
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if (isTrick){
+        return trick
+    }else {
+        if (extraTreat != null){
+            println(extraTreat(5))
+        }
+        return treat
+    }
+}
+val trick = {
+    println("No treats")
+}
+//function with lambda expression
+val treat: () -> Unit = { //specifying treat variable's datatype as () -> Unit
+    println("Have a treat")
 }

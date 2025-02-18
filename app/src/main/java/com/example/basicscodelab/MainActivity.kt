7085f6b32c3d043feb91a2a48aad13e3b2418bb9
Angela <nyabutiangella@gmail.com>
2025-02-18 10:21:30 +0300

    State hoisting

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,8 +18,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,19 +31,59 @@ import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge()//ensures the content extends to the edge of the screen
         setContent {
-            BasicsCodelabTheme {
-                MyApp(modifier = Modifier.fillMaxSize())
+            BasicsCodelabTheme {//applies custom theme
+                MyApp(modifier = Modifier.fillMaxSize()) //root composable function that determines what is displayed
             }
+        }
+    }
+}
+@Composable
+fun OnboardingScreen( //first screen of the app
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier) {
+
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked) {
+            Text("Continue")
+
+        }
+
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    //hoisting a state
+    var shouldShowOnboarding by remember { mutableStateOf(true) } //Ensures the state persists through recompositions
+    Surface(modifier) {
+        if (shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
         }
     }
 }
 
 @Composable
-fun MyApp (
-           modifier: Modifier = Modifier,
-           names: List<String> = listOf("World", "Compose")
+private fun Greetings(//displays a list of names
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
 ) {
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
@@ -47,10 +91,18 @@ fun MyApp (
         }
     }
 }
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember{ mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+fun GreetingsPreview() {
+    BasicsCodelabTheme {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) { //displays a single name
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp),color = MaterialTheme.colorScheme.primary) {
         Row(modifier = Modifier.padding(24.dp)) {//you can chain modifiers together
@@ -66,8 +118,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 )
             }
             ElevatedButton(
-                onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                onClick = { expanded = !expanded }) {
+                Text(if (expanded) "Show less" else "Show more")
             }
 
         }
@@ -79,5 +131,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BasicsCodelabTheme {
         MyApp()
+    }
+}
+@Preview
+@Composable
+fun MyAppPreview() {
+    BasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }

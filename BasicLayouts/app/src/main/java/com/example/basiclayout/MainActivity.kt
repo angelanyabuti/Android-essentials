2d472738cd6304c6e67a683890062d35f4df1043
Angela <nyabutiangella@gmail.com>
2025-02-21 17:08:08 +0300

    Alignment. Aligning items in the body.

--- BasicLayouts/app/src/main/java/com/example/basiclayout/MainActivity.kt
+++ BasicLayouts/app/src/main/java/com/example/basiclayout/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -17,7 +24,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +41,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   SearchBar()
+                    SearchBar(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -65,6 +76,46 @@ fun SearchBar (modifier: Modifier = Modifier) {
 fun SearchBarPreview() {
     BasicLayoutTheme {
         SearchBar()
+    }
+}
+//Body Element
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ){
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(text),
+            modifier = Modifier.paddingFromBaseline(
+                top = 24.dp, bottom = 8.dp
+            ),
+            style = MaterialTheme.typography.bodyMedium
+        )
+
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun AlignYourBodyElementPreview() {
+    BasicLayoutTheme {
+        AlignYourBodyElement(
+            text = R.string.ab1_invertions,
+            drawable = R.drawable.ab1,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
 

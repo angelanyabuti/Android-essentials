8ebd0e6122e09c68936168adb27de4546bf5d8c1
Angela <nyabutiangella@gmail.com>
2025-02-24 08:58:20 +0300

    Adding scrolling to the home page.

--- BasicLayouts/app/src/main/java/com/example/basiclayout/MainActivity.kt
+++ BasicLayouts/app/src/main/java/com/example/basiclayout/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,7 +25,9 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -53,15 +56,7 @@ class MainActivity : ComponentActivity() {
             BasicLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding)) {
-
-                        SearchBar()
-                        HomeSection(title = R.string.align_your_body) {
-                            AlignYourBodyRow()
-                        }
-                        HomeSection(title = R.string.favorite_collections) {
-                            FavoriteCollectionsGrid()
-                        }
-
+                        HomeScreen()
                     }
                 }
             }
@@ -262,6 +257,32 @@ fun HomeSectionPreview() {
         HomeSection(R.string.align_your_body) {
             AlignYourBodyRow()
         }
+    }
+}
+
+//Combining the building blocks into a full screen
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier){
+    Column (
+        modifier
+            .verticalScroll(rememberScrollState())
+    ){
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ScreenPreview() {
+    BasicLayoutTheme {
+        HomeScreen()
     }
 }
 

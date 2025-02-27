dfae1a5a6e319a1faa2563092a24244b0fe623fa
Angela <nyabutiangella@gmail.com>
2025-02-27 14:29:09 +0300

    State in compose.

--- State/app/build.gradle.kts
+++ State/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.basicstatecodelab"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.basicstatecodelab"

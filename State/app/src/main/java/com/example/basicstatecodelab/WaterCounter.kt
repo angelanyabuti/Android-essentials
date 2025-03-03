1603e996a3f1af1542f39547e747bdb1c1084bb4
Angela <nyabutiangella@gmail.com>
2025-03-03 21:35:15 +0300

    Using rememberSaveable to retain state across recompositions.

--- State/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ State/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -19,30 +20,17 @@ fun WaterCounter(modifier: Modifier = Modifier) {
 
     Column(modifier = modifier.padding(16.dp)) {
         //changes to count are now tracked by compose
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false }, //when this button is clicked, showTask is set to false and is not displayed
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
+
             //This text is present when the button is clicked and count is greater than 0
             //And absent otherwise
             Text(
                 text = "You've had $count glasses.",
             )
         }
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(onClick = { count++ }, enabled = count < 10) {
+            Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
                 Text("Add one")
-
             }
-            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
-                Text("Clear water count")
-            }
-        }
-
     }
 }
